5291762bb7489457b8a85f2bde8f9e48041cf09c
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2017-07-11 14:18:24 +0200

    split kernels in 2 versions, wrote separate tests

--- predict_model_snippet.cu
+++ predict_model_snippet.cu
@@ -3,17 +3,62 @@
 
 /* sum: 2x1 array */
 
+
+
+/*
+ * kernel_array_beam_slave_sincos kernel that performs the reduction manually, instead of using a library
+ *
+ * This kernel can be executed with any number of threads per block, as long as it's a power of 2
+ * The number of threads per block is however unrelated to N
+ * This kernel is also intended to be executed with only a single thread block
+ */
 extern "C"
 __global__ void 
-kernel_array_beam_slave_sincos(int N, float r1, float r2, float r3, float *x, float *y, float *z, float *sum, int blockDim_2) {
-  unsigned int n=threadIdx.x; //+blockDim.x*blockIdx.x;
-  //extern __shared__ float tmpsum[]; /* assumed to be size 2*Nx1 */
+sincos_manual(int N, float r1, float r2, float r3, float *x, float *y, float *z, float *sum) {
+    int n=threadIdx.x;
+    __shared__ float tmpsum[2*block_size_x];
+    tmpsum[2*n]=0.0f;
+    tmpsum[2*n+1]=0.0f;
 
-  #if use_cub == 0
-  __shared__ float tmpsum[1000]; /* assumed to be size 2*Nx1 */
-  tmpsum[2*n]=0.0f;
-  tmpsum[2*n+1]=0.0f;
-  #else
+    //thread block iterates over elements 0 to N
+    //values are accumulated in shared memory
+    for (int i=n; i<N; i+=block_size_x) {
+        float ss,cc;
+        sincosf((r1*__ldg(&x[i])+r2*__ldg(&y[i])+r3*__ldg(&z[i])),&ss,&cc);
+        tmpsum[2*n] += ss;
+        tmpsum[2*n+1] += cc;
+    }
+    __syncthreads();
+
+    //reduction loop
+    #pragma unroll
+    for (unsigned int s=block_size_x/2; s>0; s>>=1) {
+        if (n < s) {
+            tmpsum[2*n] += tmpsum[2*(n+s)];
+            tmpsum[2*n+1] += tmpsum[2*(n+s)+1];
+        }
+        __syncthreads();
+    }
+
+    // now thread 0 will add up results
+    if (n==0) {
+        sum[0]=tmpsum[0];
+        sum[1]=tmpsum[1];
+    }
+
+}
+
+/*
+ * kernel_array_beam_slave_sincos kernel that performs the reduction manually, instead of using a library
+ *
+ * This kernel can be executed with any number of threads per block, recommended to use a multiple of 32
+ * The number of threads per block is unrelated to N
+ * This kernel is also intended to be executed with only a single thread block
+ */
+extern "C"
+__global__ void 
+sincos_cub(int N, float r1, float r2, float r3, float *x, float *y, float *z, float *sum) {
+    int n=threadIdx.x;
 
     // Specialize BlockReduce for a 1D block of block_size_x threads on type T
     typedef cub::BlockReduce<float, block_size_x> BlockReduce;
@@ -23,57 +68,31 @@ kernel_array_beam_slave_sincos(int N, float r1, float r2, float r3, float *x, fl
     float sin_sum = 0.0f;
     float cos_sum = 0.0f;
 
-
-  #endif
-
-  for (int i=n; i<N; i+=N) {
-    float ss,cc;
-    sincosf((r1*__ldg(&x[i])+r2*__ldg(&y[i])+r3*__ldg(&z[i])),&ss,&cc);
-    #if use_cub == 0
-    tmpsum[2*n] += ss;
-    tmpsum[2*n+1] += cc;
-    #else
-    sin_sum += ss;
-    cos_sum += cc;
-    #endif
-  }
-  __syncthreads();
-
-  #if use_cub == 0
- // Build summation tree over elements, handling case where total threads is not a power of two.
-  int nTotalThreads = blockDim_2; // Total number of threads (==N), rounded up to the next power of two
-  while(nTotalThreads > 1) {
-    int halfPoint = (nTotalThreads >> 1); // divide by two
-    if (n < halfPoint) {
-     int thread2 = n + halfPoint;
-     if (thread2 < blockDim.x) { // Skipping the fictitious threads >N ( blockDim.x ... blockDim_2-1 )
-      tmpsum[2*n] = tmpsum[2*n]+tmpsum[2*thread2];
-      tmpsum[2*n+1] = tmpsum[2*n+1]+tmpsum[2*thread2+1];
-     }
+    // accumulate in thread-local registers
+    for (int i=n; i<N; i+=block_size_x) {
+        float ss,cc;
+        sincosf((r1*__ldg(&x[i])+r2*__ldg(&y[i])+r3*__ldg(&z[i])),&ss,&cc);
+        sin_sum += ss;
+        cos_sum += cc;
     }
     __syncthreads();
-    nTotalThreads = halfPoint; // Reducing the binary tree size by two
-  }
 
-  /* now thread 0 will add up results */
-  if (threadIdx.x==0) {
-   sum[0]=tmpsum[0];
-   sum[1]=tmpsum[1];
-  }
-
-  #else
+    //reduce using CUB library
     sin_sum = BlockReduce(temp_storage).Sum(sin_sum);
-    __syncthreads();
+    __syncthreads(); // because temp_storage is reused, sync is needed here
     cos_sum = BlockReduce(temp_storage).Sum(cos_sum);
 
-    /* now thread 0 will add up results */
-    if (threadIdx.x==0) {
+    //thread 0 stores per-thread block result
+    if (n==0) {
         sum[0]=sin_sum;
         sum[1]=cos_sum;
     }
-  #endif
 
 }
+
+
+
+
 // Old kernel
 extern "C"
 __global__ void 
--- tune_kernel_array_beam_slave_sincos.py
+++ tune_kernel_array_beam_slave_sincos.py
@@ -4,56 +4,94 @@ import kernel_tuner
 from collections import OrderedDict
 
 def power_bit_length(x):
-    return 2**(x-1).bit_length()
+    return 2**(int(x-1).bit_length())
 
-def tune():
+def get_input_data(Nelem):
+    # r1, r2, r3 = np.random.rand(3).astype(np.float32)
+    r1, r2, r3 = np.linspace(0.1,1,3, endpoint=False).astype(np.float32)
+    # x, y, z    = np.random.rand(3, Nelem).astype(np.float32)
+    x, y, z = np.linspace(0.1,1,3*Nelem, endpoint=False).astype(np.float32).reshape(3, Nelem)
+    tar        = np.empty(2).astype(np.float32)
+    return np.int32(Nelem), r1, r2, r3, x, y, z, tar
+
+
+def call_reference_kernel(Nelem, r1, r2, r3, x, y, z, tar):
+    with open('predict_model_snippet.cu', 'r') as f:
+        kernel_string = f.read()
+    blockDim_2 = np.int32(power_bit_length(Nelem))
+    args = [np.int32(Nelem), r1, r2, r3, x, y, z, tar, blockDim_2]
+    params = {"block_size_x": int(Nelem)}
+    reference = kernel_tuner.run_kernel("kernel_array_beam_slave_sincos_original", kernel_string, 1,
+        args, params, grid_div_x=[])
+    return reference[7]
+
+
+def test_manual_kernel():
+    with open('predict_model_snippet.cu', 'r') as f:
+        kernel_string = f.read()
+    Nelem = 500
+    args = get_input_data(Nelem)
+    reference = call_reference_kernel(*args)
+
+    params = {"block_size_x": 32}
+    answer = kernel_tuner.run_kernel("sincos_manual", kernel_string, 1,
+        args, params, grid_div_x=[])
+    answer = answer[7]
+
+    assert np.allclose(reference, answer, atol=1e-6)
+
+
+def test_cub_kernel():
+    with open('predict_model_snippet.cu', 'r') as f:
+        kernel_string = f.read()
+    Nelem = 500
+    args = get_input_data(Nelem)
+    reference = call_reference_kernel(*args)
+
+    params = {"block_size_x": 192}
+    answer = kernel_tuner.run_kernel("sincos_cub", kernel_string, 1,
+        args, params, grid_div_x=[])
+    answer = answer[7]
+
+    assert np.allclose(reference, answer, atol=1e-6)
+
+
+def tune_kernels():
 
     with open('predict_model_snippet.cu', 'r') as f:
         kernel_string = f.read()
 
     # problem_size = (4096, 2048)
     Nelem = 500
+    args = get_input_data(Nelem)
     # N: no of stations
     N     =  61
     K     = 1e4
     T     =   1
     F     =   1
 
+    # Tell the Kernel Tuner how the grid dimensions are to be computed
     problem_size = 1
-
-    #  ThreadsPerBlock = 8
-
-    # grid_size = int(2 * (problem_size + ThreadsPerBlock - 1)/ThreadsPerBlock)
-
-    # size = numpy.prod(problem_size)
-
-    # r1, r2, r3 = np.random.rand(3).astype(np.float32)
-    r1, r2, r3 = np.linspace(0.1,1,3, endpoint=False).astype(np.float32)
-    # x, y, z    = np.random.rand(3, Nelem).astype(np.float32)
-    x, y, z = np.linspace(0.1,1,3*Nelem, endpoint=False).astype(np.float32).reshape(3, Nelem)
-    tar        = np.empty(2).astype(np.float32)
-    blockDim_2 = power_bit_length(Nelem)
-
-    args = [np.int32(Nelem), r1, r2, r3, x, y, z, tar, np.int32(blockDim_2)]
-
-    tune_params = OrderedDict()
-    tune_params["block_size_x"] = [2**i for i in range (7,11)]
-    tune_params["use_cub"] = [0, 1]
-    # tune_params["use_cub"] = [1, 0]
-
     grid_div_x = []
 
-    # Verify results using the old kernel.
-    params = { "block_size_x": 128}
-    results = kernel_tuner.run_kernel("kernel_array_beam_slave_sincos_original", kernel_string, problem_size,
-        args, params, grid_div_x=grid_div_x)
+    # Compute reference answer using the original kernel
+    reference = call_reference_kernel(*args)
+    answer = [None, None, None, None, None, None, None, reference]
 
-    answer = [None, None, None, None, None, None, None, results[7], None]
+    # Tune the kernel with the manual reduction loop
+    tune_params = OrderedDict()
+    tune_params["block_size_x"] = [2**i for i in range (5,11)]
+    manual_kernel, _ = kernel_tuner.tune_kernel("sincos_manual", kernel_string, problem_size,
+        args, tune_params, grid_div_x=grid_div_x, verbose=True, answer=answer)
 
-    return kernel_tuner.tune_kernel("kernel_array_beam_slave_sincos", kernel_string, problem_size,
-        args, tune_params, grid_div_x=grid_div_x,
-        verbose = True, answer = answer)
+    # Tune the kernel that uses CUB for reductions
+    tune_params["block_size_x"] = [32*i for i in range (1,33)]
+    cub_kernel, _ = kernel_tuner.tune_kernel("sincos_cub", kernel_string, problem_size,
+        args, tune_params, grid_div_x=grid_div_x, verbose=True, answer=answer)
 
+    return manual_kernel, cub_kernel
 
 if __name__ == "__main__":
-    print(tune())
+    test_manual_kernel()
+    test_cub_kernel()
+    tune_kernels()
