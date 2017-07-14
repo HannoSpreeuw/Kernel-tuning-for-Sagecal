66e58f1442999e5e225ff1e5156348bdf5f8c3d6
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2017-07-14 13:33:07 +0200

    tuning kernel_array_beam

---
+++ predict_model.h
@@ -0,0 +1,51 @@
+/* soure types */
+#define STYPE_POINT 0
+#define STYPE_GAUSSIAN 1
+#define STYPE_DISK 2
+#define STYPE_RING 3
+#define STYPE_SHAPELET 4
+
+#ifndef CONST_C
+#define CONST_C 299792458.0
+#endif
+#ifndef DEFAULT_TH_PER_BK
+#define DEFAULT_TH_PER_BK 64
+#endif
+
+typedef struct exinfo_gaussian_ {
+  double eX,eY,eP; /* major,minor,PA */
+
+  double cxi,sxi,cphi,sphi; /* projection of [0,0,1] to [l,m,n] */
+  int use_projection;
+} exinfo_gaussian;
+
+typedef struct exinfo_disk_ {
+  double eX; /* diameter */
+
+  double cxi,sxi,cphi,sphi; /* projection of [0,0,1] to [l,m,n] */
+  int use_projection;
+} exinfo_disk;
+
+typedef struct exinfo_ring_ {
+  double eX; /* diameter */
+
+  double cxi,sxi,cphi,sphi; /* projection of [0,0,1] to [l,m,n] */
+  int use_projection;
+} exinfo_ring;
+
+typedef struct exinfo_shapelet_ {
+  int n0; /* model order, no of modes=n0*n0 */
+  double beta; /* scale*/
+  double *modes; /* array of n0*n0 x 1 values */
+  double eX,eY,eP; /* linear transform parameters */
+
+  double cxi,sxi,cphi,sphi; /* projection of [0,0,1] to [l,m,n] */
+  int use_projection;
+} exinfo_shapelet;
+
+typedef struct baseline_t_ {
+ int sta1,sta2;
+ unsigned char flag; /* if this baseline is flagged, set to 1, otherwise 0: 
+             special case: 2 if baseline is not used in solution, but will be
+              subtracted */
+} baseline_t;
---
+++ tune_kernel_array_beam.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+from collections import OrderedDict
+import os
+
+import numpy as np
+from kernel_tuner import tune_kernel, run_kernel
+
+def get_kernel_path():
+    """ get path to the kernels as a string """
+    return str(os.path.dirname(os.path.realpath(__file__)))+'/'
+
+
+def generate_input_data(N, T, K, F):
+    """ Generate random input data for calling kernel_array_beam
+    N: no of stations
+    T: no of time slots
+    K: no of sources
+    F: no of frequencies
+    freqs: frequencies Fx1
+    longitude, latitude: Nx1 station locations
+    time_utc: Tx1 time
+    Nelem: Nx1 array of no. of elements
+    x,y,z: N*Nelemsx1 array of station locations
+    ra,dec: Kx1 source positions
+    beam: output beam values NxTxKxF values
+    ph_ra0,ph_dec0: beam pointing direction
+    ph_freq0: beam referene freq
+    """
+
+    freqs = np.random.randn(F).astype(np.float32)
+    longitude = np.random.randn(N).astype(np.float32)
+    latitude = np.random.randn(N).astype(np.float32)
+    time_utc = np.random.randn(T).astype(np.float64)
+    Nelem = (500+5.*np.random.randn(N)).astype(np.int32)
+
+    print(Nelem)
+    TotalElem = np.sum(Nelem)
+    print(TotalElem)
+
+    x, y, z = np.random.randn(3, TotalElem).astype(np.float32)
+    ra, dec = np.random.randn(2, K).astype(np.float32)
+    beam = np.zeros(N*T*K*F).astype(np.float32)
+    ph_ra0, ph_dec0, ph_freq0 = np.random.randn(3).astype(np.float32)
+
+    return (np.int32(N), np.int32(T), np.int32(K), np.int32(F), freqs, longitude, latitude,
+           time_utc, Nelem, x, y, z, ra, dec, ph_ra0, ph_dec0, ph_freq0, beam, np.int32(TotalElem))
+
+
+def tune():
+
+    N = 61
+    T = 200
+    K = 150
+    F = 10
+
+    args = generate_input_data(N, T, K, F)
+
+    problem_size = N*T*K*F
+
+    tune_params = OrderedDict()
+    tune_params["block_size_x"] = [2**i for i in range(5,11)]
+    tune_params["use_kernel"] = [0, 1]
+
+    cp = ["-rdc=true", "-arch=sm_52", "-I"+get_kernel_path()]
+    params = {"block_size_x": 32, "use_kernel": 1}
+    answer = run_kernel("kernel_tuner_host_array_beam", [get_kernel_path()+"predict_model.cu"], problem_size, args, params,
+                lang="C", compiler_options=cp)
+    ref = [None for _ in answer]
+    ref[17] = answer[17]
+
+    #restrict = ["use_kernel == 0 or block_size_x<=64"]
+    results, env = tune_kernel("kernel_tuner_host_array_beam", [get_kernel_path()+"predict_model.cu"], problem_size, args, tune_params,
+                lang="C", compiler_options=cp, verbose=True, answer=ref)
+
+
+    return results
+
+
+
+if __name__ == "__main__":
+    tune()
