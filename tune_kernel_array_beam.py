b67321ada9205bfbb2aaa7aef078bc512e81a7f8
HannoSpreeuw <h.spreeuw@esciencecenter.nl>
2017-09-14 15:41:38 +0200

    Reading in binary files from a sagecal run on fs5

--- tune_kernel_array_beam.py
+++ tune_kernel_array_beam.py
@@ -68,6 +68,58 @@ def generate_input_data(N, T, K, F):
     return (np.int32(N), np.int32(T), np.int32(K), np.int32(F), freqs, longitude, latitude,
            time_utc, Nelem, x, y, z, ra, dec, ph_ra0, ph_dec0, ph_freq0, beam, np.int32(TotalElem))
 
+def load_real_data():
+    freqs = np.random.randn(F).astype(np.float32)
+    longitude = np.random.randn(N).astype(np.float32)
+    latitude = np.random.randn(N).astype(np.float32)
+    time_utc = np.random.randn(T).astype(np.float64)
+
+    elem_per_station = 500
+    max_elem = 512
+    Nelem = (elem_per_station+5.*np.random.randn(N)).astype(np.int32)
+    Nelem = np.array([i if i <= max_elem else max_elem for i in Nelem]).astype(np.int32)
+    TotalElem = np.sum(Nelem)
+
+    #it's important that TotalElem is always the same to get our
+    #measurements right across multiple runs of this script
+    target_total = N*elem_per_station
+    while TotalElem < target_total:
+        too_few = min(target_total - TotalElem, N)
+        for i in range(too_few):
+            if Nelem[i] < max_elem:
+                Nelem[i] = Nelem[i]+1
+        TotalElem = np.sum(Nelem)
+    while TotalElem > target_total:
+        too_many = min(TotalElem - target_total, N)
+        for i in range(too_many):
+            Nelem[i] = Nelem[i]-1
+        TotalElem = np.sum(Nelem)
+    print(Nelem)
+    print(TotalElem)
+    assert TotalElem == target_total
+
+    
+    x, y, z = (1e7 * np.random.randn(3, TotalElem)).astype(np.float32)
+    ra, dec = (2 * np.pi * np.random.randn(2, K)).astype(np.float32)
+    beam = (1e6 * np.random.randn(N*T*K*F)).astype(np.float32)
+    ph_ra0, ph_dec0, ph_freq0 = (2 * np.pi * np.random.randn(3)).astype(np.float32)
+    path_to_bin_files = "bin-files"
+    
+    N = np.fromfile(path_to_bin_files + "t_N.bin", np.int32)
+    T = np.fromfile(path_to_bin_files + "t_tilesz.bin", np.int32)     
+    K = np.fromfile(path_to_bin_files + "t_carr_ncl_N.bin", np.int32)      
+    F = np.fromfile(path_to_bin_files + "t_Nf.bin", np.int32)      
+    freqs = np.fromfile(path_to_bin_files + "freq_sd.bin", np.float64).astype(np.float32)      
+    longitude = np.fromfile(path_to_bin_files + "long_d.bin", np.float64).astype(np.float32)      
+    latitude = np.fromfile(path_to_bin_files + "lat_d.bin", np.float64).astype(np.float32)            
+    time_utc = np.fromfile(path_to_bin_files + "time_d.bin", np.float64)      
+    Nelem = np.fromfile(path_to_bin_files + 'Nelem_d.bin', np.int32) 
+    x = np.fromfile("xx_d.bin", np.float64).astype(np.float32)
+    y = np.fromfile("xx_d.bin", np.float64).astype(np.float32)
+    z = np.fromfile("xx_d.bin", np.float64).astype(np.float32)
+
+    return (N, T, K, F, freqs, longitude, latitude,
+           time_utc, Nelem, x, y, z, ra, dec, ph_ra0, ph_dec0, ph_freq0, beam, np.int32(TotalElem))
 def run():
     N = 61
     T = 200
